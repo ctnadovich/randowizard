34b0917a57c1d3b159a02390f7f1de3d051968fd
ctnadovich <github@nadovich.com>
2024-01-03 02:23:16 +0000

    removed EPP references

--- Views/ebrevet_faq.php
+++ Views/ebrevet_faq.php
@@ -13,7 +13,7 @@
 </p>
 
     <h3>What is eBrevet?</h3>
-    <p>eBrevet is an open-source Android/iOS app that serves as an automated brevet card providing Electronic Proof of Passage on a randonneuring brevet or permanent. It maintains some of the "feel" of the traditional paper brevet card process. The app only needs to be activated at controls and does not require Internet data service at controls. Using the phone's clock and GPS, the app determines if you are near a control (within 500 meters). If a nearby control is open, you can record your authenticated check-in within the app. When network access happens to be available, the app will report these control check in times to the PA Rando server, which will be visible in real time on the roster page. When the event is completed successfully, the app can automatically upload results to Randonneuring.org, and generates a unique Proof of Passage Certificate that is sharable on social media.</p>
+    <p>eBrevet is an open-source Android/iOS app that serves as an automated, self-certifying brevet card for a randonneuring brevet or permanent. It maintains some of the "feel" of the traditional paper brevet card process. The app only needs to be activated at controls and does not require Internet data service at controls. Using the phone's clock and GPS, the app determines if you are near a control (within 500 meters). If a nearby control is open, you can record your authenticated check-in within the app. When network access happens to be available, the app will report these control check in times to the PA Rando server, which will be visible in real time on the roster page. When the event is completed successfully, the app can automatically upload results to Randonneuring.org, and generates a unique brevet Completion Certificate that is sharable on social media.</p>
 
     <h3>Where can I get eBrevet?</h3>
     <p>You can find the app on both the <A HREF='https://www.apple.com/app-store/'>Apple Store</A> and the <A HREF='https://play.google.com/'>Google Play Store</a>. Search for "eBrevet" by "CTNadovich". Please make sure you are using the latest version of the app. Control checkins or event downloads will not work if your app is outdated. </p>
@@ -33,8 +33,8 @@
     <P>Three reasons:</p>
    <ol> 
 <li>You will still need a paper brevet card to START an event because the paper card has the <B>start code</B> required by eBrevet. </li>
-<li>You also still need a paper brevet card to FINISH an event. At the finish, you must write your time of arrival at the finish control, write the <b>finish code</b> from the app, and sign the paper card as proof you completed the ride and return the card to the organizer.  </li>
-<li>If your phone dies, or you have other technical problems with the app, you can switch back to the paper card for Proof of Passage and still get credit for the ride.</li></ol>
+<li>You also still need a paper brevet card to FINISH an event. At the finish, you must write your time of arrival at the finish control, write the <b>finish code</b> from the app, and sign the paper card to assert that you completed the ride and return the card to the organizer.  </li>
+<li>If your phone dies, or you have other technical problems with the app, you can switch back to the paper card and still get credit for the ride.</li></ol>
 
 <h2>Using eBrevet</h2>
 
@@ -93,7 +93,7 @@
 <p>If you are physically near (&LT;500 meters) a control at the correct time (after the control opens, before it closes) a "Check-In" button should appear next to the control on the app. Sometimes it takes the GPS in your phone  a few seconds to update your location. If you are impatient, press the "GPS UPDATE" button. If the "Check-In" button still doesn't appear, maybe you've disallowed Location Permissions for the eBrevet app. Check your phone Settings and make sure that eBrevet has LOCATION permission. </p>
 
 <h3>But what if the app breaks or a dog eats my phone?</h3>
-<p>Yes cell phones do sometimes fail, and software does sometimes have bugs. Prudent riders using eBrevet also write onto their paper brevet card their time of arrival and "control check in codes"  from the app at each intermediate control  as a paper backup.  Writing down your time and these codes isn't required -- it's simply prudent. If you think your phone is about to fail, battery die, or the app is acting wonky, record all check-in codes and be ready switch to old-school brevet card control check-in processes (get merchant initials, receipts, etc...) for the remainder of the event. The RBA will likely not accept 'my phone died' and a blank card as proof of passage. </p>
+<p>Yes cell phones do sometimes fail, and software does sometimes have bugs. Prudent riders using eBrevet also write onto their paper brevet card their time of arrival and "control check in codes"  from the app at each intermediate control  as a paper backup.  Writing down your time and these codes isn't required -- it's simply prudent. If you think your phone is about to fail, battery die, or the app is acting wonky, record all check-in codes and be ready switch to old-school brevet card control check-in processes (get merchant initials, receipts, etc...) for the remainder of the event. The RBA will likely not accept 'my phone died' and a blank card as evidence you completed the event. </p>
 
 <h3>What if I find a bug or want to suggest an improvement?</h3>
 <p>You can <A HREF='https://github.com/ctnadovich/ebrevet/issues'>open 
@@ -101,7 +101,7 @@
 
 <h2>Conclusion</h2>
 
-<p>Enjoy the blessings of technology in this app, but keep in mind that this is still randonneuring. Per our traditions, you as a rider must check-in at controls and must provide the required proof of passage for your ride.</p>
+<p>Enjoy the blessings of technology in this app, but keep in mind that this is still randonneuring. Per our traditions, you as a rider must check-in at controls and must provide the required documentation for your ride.</p>
 
 
 

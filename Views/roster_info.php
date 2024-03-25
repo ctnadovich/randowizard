dcd8dc14097009343a36f6fea5dad57724d04fc8
ctnadovich <github@nadovich.com>
2024-03-25 13:52:44 +0000

    roster display improvements

---
+++ Views/roster_info.php
@@ -0,0 +1,10 @@
+<div class="w3-container">
+
+ 
+    <h2><?= $title ?> <A class='w3-button' 
+    HREF=<?=site_url("event_info/$event_code")?>><i class="fa-solid fa-circle-info"></i></A></h2>
+    <div class="w3-container w3-padding w3-responsive" style="clear: right;">
+
+        <TABLE CLASS='w3-table-all w3-centered'><?= $roster_table ?></TABLE>
+    </div>
+</div>

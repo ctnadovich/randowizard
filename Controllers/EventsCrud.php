7c6e0d206f6b925b0117b716c4f164705d158303
ctnadovich <github@nadovich.com>
2024-03-25 13:51:16 +0000

    Full list of info pages in column

--- Controllers/EventsCrud.php
+++ Controllers/EventsCrud.php
@@ -97,14 +97,14 @@ class EventsCrud extends BaseController
             case 'future':
                 $crud->where('start_datetime >=', $now);
                 // $crud->orderBy('date','asc');
-                $crud->defaultOrdering('start_datetime','asc');
+                $crud->defaultOrdering('start_datetime', 'asc');
                 $title = "Future Events";
                 $crud->setSubject('Future Event', 'Future Events');
                 break;
             case 'past':
                 $crud->where('start_datetime <', $now);
                 // $crud->order_by('date','desc');
-                $crud->defaultOrdering('start_datetime','desc');
+                $crud->defaultOrdering('start_datetime', 'desc');
                 $title = "Past Events";
                 $crud->setSubject('Past Event', 'Past Events');
                 break;
@@ -167,7 +167,7 @@ class EventsCrud extends BaseController
         $crud->displayAs('distance', 'Official Dist (km)');
         $crud->displayAs('region_id', 'Region');
         $crud->displayAs('gravel_distance', 'Official Gravel (km)');
-        $crud->displayAs('event_info', 'Info');
+        $crud->displayAs('event_info', 'Info Pages');
         $crud->displayAs('cue_version', 'Version');
 
         $crud->callbackAfterUpdate([$this, '_set_last_change_time']);
@@ -179,22 +179,9 @@ class EventsCrud extends BaseController
         return $this->load_view(['crud_output']);
     }
 
-    public function update_nine()
-    {
-        $stateParameters = new stdClass;
-        $stateParameters->primaryKeyValue = 9;
-        $stateParameters = $this->_set_last_change_time($stateParameters);
-        $this->die_info(__METHOD__, print_r($stateParameters, true));
-    }
 
-    public function _set_last_change_time($stateParameters)
-    {
-        $eventModel = model('Event');
-        $now = (new \DateTime('now', (new \DateTimeZone('UTC'))))->format('Y-m-d H:i:s');
-        $id = $stateParameters->primaryKeyValue;
-        $eventModel->update($id, ['last_changed' => "$now"]);
-        return $stateParameters;
-    }
+    // CALLBACK COLUMNS
+
 
     public function _event_code($value, $row)
     {
@@ -206,24 +193,89 @@ class EventsCrud extends BaseController
         return $event_code;
     }
 
-    public function _roster_url($value, $row)
+    public function _status_icons($value, $row)
     {
+        $attribs = explode(',', $value);
+
+        $d = "";
+
+        foreach ($attribs as $a) {
+            $icon = ((!empty($this->status_icon[$a])) ? $this->status_icon[$a] : "$a ");
+            $d .= "<span title='$a'>$icon</span>";
+        }
+
+        return $d;
+    }
+
+
+    public function _paperwork($value, $row)
+    {
+        if (empty($row->route_url)) return "No Route";
 
         $event_id = $row->id;
         $region_id = $row->region_id;
         $event_code = "$region_id-$event_id";
-        $roster_url = site_url("roster/$event_code");
-        $roster_upload_url = site_url("roster_upload/$event_code");
-        $n_riders = $this->rosterModel->n_riders($event_id); // =$value; //$this->truncate($value,30);
-
-
-        return <<<EOT
-<span style="width:100%;text-align:center;display:block;">
-<A HREF='$roster_url' TITLE='Roster'><i class='fas fa-users' style='color: blue;'></i>($n_riders)</A> 
-<A HREF='$roster_upload_url' TITLE='Upload'><i class='fas fa-upload' style='color: blue;'></i></A>
-</span>
+        // $wizard_url = site_url("route_manager/$event_code");
+        $dropdown = <<<EOT
+<div class="w3-dropdown-hover">
+<button class="w3-button w3-blue">Download&nbsp;<i class="fa-solid fa-download"></i></button>
+<div class="w3-dropdown-content w3-bar-block w3-border">
 EOT;
+
+        $drop_items = [
+            ["generate/$event_code/signin_sheet", "Sign-In Sheet (for all rider)"],
+            ["generate/$event_code/card_outside_roster", "Brevet Card Outsides (cards for all riders)"],
+            ["generate/$event_code/card_outside_blank", "Brevet Card Outside (blank rider name)"],
+            ["generate/$event_code/card_inside", "Brevet Card Inside"],
+            // ["roster_upload/$event_code", "Upload a Roster (CoM CSV Format)"],
+            ["generate/$event_code/rusacsv", "Download Results (RUSA CSV Format)"],
+        ];
+
+        foreach ($drop_items as $i) {
+            list($url, $desc) = $i;
+            $url = site_url($url);
+            $dropdown .=  "<A class='w3-bar-item w3-button' HREF='$url'>$desc</A>";
+        }
+        $dropdown .= "</div></div>";
+
+
+        return $dropdown;
+
+        // "<A class='w3-button w3-light-gray w3-round' HREF='$wizard_url'>
+        // Cue Wizard</A>";
     }
+
+    public function _event_info($value, $row)
+    {
+        $event_id = $row->id;
+        $region_id = $row->region_id;
+        $event_code = "$region_id-$event_id";
+
+        $dropdown = <<<EOT
+<div class="w3-dropdown-hover">
+<button class="w3-button w3-blue">Info&nbsp;<i class="fa-solid fa-info-circle"></i></button>
+<div class="w3-dropdown-content w3-bar-block w3-border">
+EOT;
+
+$drop_items = [
+    ["event_info/$event_code", "<i class='fa-solid fa-info-circle' style='color: blue;'></i> Event Info"],
+    ["roster_info/$event_code", "<i class='fas fa-users' style='color: blue;'></i> Roster"],
+    ["checkin_status/$event_code", "<i class='fas fa-list-check' style='color: blue;'></i> Check-ins"],
+];
+
+foreach ($drop_items as $i) {
+    list($url, $desc) = $i;
+    $url = site_url($url);
+    $dropdown .=  "<A class='w3-bar-item w3-button' HREF='$url'>$desc</A>";
+}
+$dropdown .= "</div></div>";
+
+
+return $dropdown;
+
+
+    }
+
 
     public function _roster_manage($value, $row)
     {
@@ -247,8 +299,8 @@ EOT;
             ["roster/$event_code", "<i class='fas fa-users' style='color: blue;'></i> Manage Roster"],
             ["vet_roster/$event_code", "<i class='fas fa-users' style='color: blue;'></i> Check RUSA Membership"],
             ["roster_upload/$event_code", "<i class='fas fa-upload' style='color: blue;'></i> Upload Roster"],
-            ["checkin_manage/$event_code", "<i class='fas fa-check' style='color: blue;'></i> Manage Checkins"],
-            
+            ["checkin_manage/$event_code", "<i class='fas fa-check' style='color: blue;'></i> Raw Checkin Data"],
+
         ];
 
         foreach ($drop_items as $i) {
@@ -265,27 +317,88 @@ EOT;
         // Cue Wizard</A>";
     }
 
-    public function vet_roster($event_code){
+    public function _route($value, $row)
+    {
+
+        if (empty($row->route_url)) return "No Route";
+
+        $event_id = $row->id;
+        $region_id = $row->region_id;
+        $event_code = "$region_id-$event_id";
+        $wizard_url = site_url("route_manager/$event_code");
+
+
+        return <<<EOT
+        <div class='w3-container w3-center' style="background-color:rgb(206,206,206);">
+<A HREF='$wizard_url' class='w3-button w3-blue'>Wizard&nbsp;<i class='fa-solid fa-hat-wizard'></i></A>
+</div>
+EOT;
+    }
+
+
+
+    // SUPPORT FUNCTIONS
+
+
+    public function update_nine()
+    {
+        $stateParameters = new stdClass;
+        $stateParameters->primaryKeyValue = 9;
+        $stateParameters = $this->_set_last_change_time($stateParameters);
+        $this->die_info(__METHOD__, print_r($stateParameters, true));
+    }
+
+    public function _set_last_change_time($stateParameters)
+    {
+        $eventModel = model('Event');
+        $now = (new \DateTime('now', (new \DateTimeZone('UTC'))))->format('Y-m-d H:i:s');
+        $id = $stateParameters->primaryKeyValue;
+        $eventModel->update($id, ['last_changed' => "$now"]);
+        return $stateParameters;
+    }
+
+    public function _roster_url($value, $row)
+    {
+
+        $event_id = $row->id;
+        $region_id = $row->region_id;
+        $event_code = "$region_id-$event_id";
+        $roster_url = site_url("roster/$event_code");
+        $roster_upload_url = site_url("roster_upload/$event_code");
+        $n_riders = $this->rosterModel->n_riders($event_id); // =$value; //$this->truncate($value,30);
+
+
+        return <<<EOT
+<span style="width:100%;text-align:center;display:block;">
+<A HREF='$roster_url' TITLE='Roster'><i class='fas fa-users' style='color: blue;'></i>($n_riders)</A> 
+<A HREF='$roster_upload_url' TITLE='Upload'><i class='fas fa-upload' style='color: blue;'></i></A>
+</span>
+EOT;
+    }
+
+
+    public function vet_roster($event_code)
+    {
         $event = $this->eventModel->eventByCode($event_code);
         $cutoff_datetime = $this->eventModel->getCutoffDatetime($event);
 
-        extract( $this->eventModel->parseEventCode($event_code) );
+        extract($this->eventModel->parseEventCode($event_code));
         $roster_in = $this->rosterModel->registered_riders($local_event_id);
-        $n_riders=count($roster_in);
+        $n_riders = count($roster_in);
         $rusaModel = model('Rusa');
-        $bad_riders = 0; 
+        $bad_riders = 0;
 
         $table_body = '';
-        foreach ($roster_in as $r){
-            $rusa_id=$r['rusa_id'];
-            $first_name=$r['first_name'];
-            $last_name=$r['last_name'];
+        foreach ($roster_in as $r) {
+            $rusa_id = $r['rusa_id'];
+            $first_name = $r['first_name'];
+            $last_name = $r['last_name'];
             $status = $rusaModel->rusa_status_at_date($rusa_id, $last_name, $cutoff_datetime);
-            if(is_string($status)){ // Vetting failed
+            if (is_string($status)) { // Vetting failed
                 $table_body .= "<TR class='w3-red'><TD style='width: 25%'>$first_name $last_name</TD>";
                 $table_body .= "<TD style='word-wrap: break-word;'> $status</TD>";
                 $bad_riders++;
-            }else{
+            } else {
                 $table_body .= "<TR><TD>$first_name $last_name</TD>";
                 $table_body .= "<TD>" . $status['rusa_expires_datetime']->format('Y-m-d') . "</TD>";
             }
@@ -293,12 +406,11 @@ EOT;
         }
 
         // $this -> die_message(__METHOD__, $table_body);
-        $this->viewData =  array_merge( $this->viewData, ['bad_riders'=>$bad_riders, 'n_riders'=>$n_riders, 'table_body' => $table_body]);
+        $this->viewData =  array_merge($this->viewData, ['bad_riders' => $bad_riders, 'n_riders' => $n_riders, 'table_body' => $table_body]);
 
 
         $output = $this->load_view('vet_roster');
         return $output;
-
     }
 
 
@@ -396,7 +508,7 @@ EOT;
             // Standardize the header and remove unrecognized columns
             $header = preg_filter(array_values($fnp), array_keys($fnp), $header);
             if (empty($header)) throw new \Exception("No recognized header fields.");
- 
+
 
             // Find all unique column names
             $field_j = array_flip($header);
@@ -408,13 +520,13 @@ EOT;
             if (!empty($errors)) throw new \Exception("Can't continue.");
 
             // RUSA Vetting will add the expiration field
-            $header[]='expires';
-            $header[]='checked_by';
-            
+            $header[] = 'expires';
+            $header[] = 'checked_by';
+
 
             // Process each data row
             $roster = [];
-            $line_number=0;
+            $line_number = 0;
 
             foreach ($roster_data as $r) {
                 $rider = [];
@@ -456,27 +568,25 @@ EOT;
             }
 
             $n_riders = count($roster_data);
-
         } catch (\Exception $e) {
             $message = $e->GetMessage();
             $errors[] = "Roster CSV Processing Exception: $message";
         }
 
-        if (empty($errors)){
+        if (empty($errors)) {
 
             // clear old roster
             $this->rosterModel->where('event_id', $local_event_id)->delete();
 
             // write new roster
-            foreach($roster as $rider_id => $rider_data){
-                $rc = $this->rosterModel->insert(['rider_id' => $rider_id, 'event_id'=>$local_event_id], false);
-                if($rc===false){
+            foreach ($roster as $rider_id => $rider_data) {
+                $rc = $this->rosterModel->insert(['rider_id' => $rider_id, 'event_id' => $local_event_id], false);
+                if ($rc === false) {
                     $errors[] = "Failed to save rider_id = $rider_id to roster. File upload incomplete.";
                     break;
                 }
             }
-
-        }else{
+        } else {
             $errors[] = "Errors in CSV, roster not saved.";
         }
 
@@ -485,76 +595,7 @@ EOT;
     }
 
 
-    public function _route($value, $row)
-    {
 
-        if (empty($row->route_url)) return "No Route";
-
-        $event_id = $row->id;
-        $region_id = $row->region_id;
-        $event_code = "$region_id-$event_id";
-        $wizard_url = site_url("route_manager/$event_code");
-
-
-        return <<<EOT
-        <div class='w3-container w3-center' style="background-color:rgb(206,206,206);">
-<A HREF='$wizard_url' class='w3-button w3-blue'>Wizard&nbsp;<i class='fa-solid fa-hat-wizard'></i></A>
-</div>
-EOT;
-    }
-
-    public function _event_info($value, $row)
-    {
-        $event_id = $row->id;
-        $region_id = $row->region_id;
-        $event_code = "$region_id-$event_id";
-        $wizard_url = site_url("event_info/$event_code");
-
-        return <<<EOT
-        <div class='w3-container w3-center' >
-<A HREF='$wizard_url' class='w3-button w3-blue'><i class='w3-large fa-solid fa-info-circle'></i></A>
-</div>
-EOT;
-    }
-    public function _paperwork($value, $row)
-    {
-
-        if (empty($row->route_url)) return "No Route";
-
-        $event_id = $row->id;
-        $region_id = $row->region_id;
-        $event_code = "$region_id-$event_id";
-        // $wizard_url = site_url("route_manager/$event_code");
-
-
-        $dropdown = <<<EOT
-<div class="w3-dropdown-hover">
-<button class="w3-button w3-blue">Download&nbsp;<i class="fa-solid fa-download"></i></button>
-<div class="w3-dropdown-content w3-bar-block w3-border">
-EOT;
-
-        $drop_items = [
-            ["generate/$event_code/signin_sheet", "Sign-In Sheet (for all rider)"],
-            ["generate/$event_code/card_outside_roster", "Brevet Card Outsides (cards for all riders)"],
-            ["generate/$event_code/card_outside_blank", "Brevet Card Outside (blank rider name)"],
-            ["generate/$event_code/card_inside", "Brevet Card Inside"],
-            // ["roster_upload/$event_code", "Upload a Roster (CoM CSV Format)"],
-            ["generate/$event_code/rusacsv", "Download Results (RUSA CSV Format)"],
-        ];
-
-        foreach ($drop_items as $i) {
-            list($url, $desc) = $i;
-            $url = site_url($url);
-            $dropdown .=  "<A class='w3-bar-item w3-button' HREF='$url'>$desc</A>";
-        }
-        $dropdown .= "</div></div>";
-
-
-        return $dropdown;
-
-        // "<A class='w3-button w3-light-gray w3-round' HREF='$wizard_url'>
-        // Cue Wizard</A>";
-    }
 
     private $status_icon = [
         'hidden' => "<i class='w3-text-red w3-large fas fa-mask'></i>",
@@ -562,19 +603,4 @@ EOT;
         'locked' => "<i class='w3-text-red w3-large fas fa-lock'></i>",
         'suspended' => "<i class='w3-text-red w3-large fas fa-question-circle'></i>"
     ];
-
-
-    public function _status_icons($value, $row)
-    {
-        $attribs = explode(',', $value);
-
-        $d = "";
-
-        foreach ($attribs as $a) {
-            $icon = ((!empty($this->status_icon[$a])) ? $this->status_icon[$a] : "$a ");
-            $d .= "<span title='$a'>$icon</span>";
-        }
-
-        return $d;
-    }
 }

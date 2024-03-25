204073fa8af124f194b9d69f3b998142db4937d4
ctnadovich <github@nadovich.com>
2024-03-25 13:54:18 +0000

    links to roster and checkin

--- Views/event_head.php
+++ Views/event_head.php
@@ -1,6 +1,6 @@
-
-<h2><?= $title ?> <A class='w3-button' TITLE='Rider Status'
-HREF=<?=site_url("roster_info/$event_code")?>><i class="fa-solid fa-users"></i></A></h2>
-<?php if(!empty($status_text)) echo "<h2 class='w3-text-orange'>$status_text</h2>";?>
+<h2><?= $title ?>&nbsp;
+<A class='w3-button' TITLE='Rider Status' HREF=<?= site_url("roster_info/$event_code") ?>><i class="fa-solid fa-users"></i></A>
+<A class='w3-button' TITLE='Check Ins' HREF=<?= site_url("checkin_status/$event_code") ?>><i class="fa-solid fa-list-check"></i></A>
+</h2>
+<?php if (!empty($status_text)) echo "<h2 class='w3-text-orange'>$status_text</h2>"; ?>
 <div class='w3-panel w3-margin'><?= $event_description ?></div>
-

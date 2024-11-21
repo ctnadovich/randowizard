c230807c1ecd70c721e7460f4ee40ee15a9491b2
ctnadovich <github@nadovich.com>
2024-11-21 15:19:14 +0000

    Bumping minimum version to 1.3.2

--- Controllers/EventProcessor.php
+++ Controllers/EventProcessor.php
@@ -58,7 +58,7 @@ class EventProcessor extends BaseController
 		$this->cryptoLibrary = new \App\Libraries\Crypto();
 	}
 
-	public $minimum_app_version = '1.2.4';
+	public $minimum_app_version = '1.3.2';
 
 
 
